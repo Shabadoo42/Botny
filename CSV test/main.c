81eba1ede444c9286544ad7d4a4dc582c077b7ac
Shabadoo42 <l.m.debruyne@gmail.com>
2016-11-22 14:05:46 -0800

    Working CSV parsing code
    Working CSV parsing code

--- CSV test/main.c
+++ CSV test/main.c
@@ -14,62 +14,66 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <wiringPi.h>	// Needed to use GPIO pins
+#include <wiringPi.h>
 
 struct data {
-	char field[10];
+	char field[255];
 	int value;
 }CSVdata;
 
-int readCSVData();
+void readCSVdata();
+int checkToken(char *token, char *delimiter);
 
 int main() {
+	printf("Setting up GPIO:");
 	wiringPiSetupGpio();				// Setup GPIO using CPU names
 	const unsigned char GPIOpin = 18;	// LED connected to GPIO pin 18
 	pinMode(GPIOpin, OUTPUT);			// GPIO pin 18 output
-
-	readCSVData();
-
-	unsigned short blinkDelay;
-
-	if(strcmp(CSVdata.field, "blinkDelay") == 0) {
-		blinkDelay = CSVdata.value;
-	}
-	else {
-		printf("Error: CSV file incorrect\n");
-		return 0;
-	}
-
-	printf("LED blinking\n");
-	printf("Press ctrl + C to exit\n");
+	digitalWrite(GPIOpin, 0);
+	printf(" Done.\n");
 
 	while(1) {
+		readCSVdata();
+		printf("Press ctrl + C to exit.\n");
 		digitalWrite(GPIOpin, 1);	// LED on
-		delay(blinkDelay);			// Time on
+		delay(CSVdata.value);		// Time on
 		digitalWrite(GPIOpin, 0);	// LED off
-		delay(blinkDelay);			// Off time
+		delay(CSVdata.value);		// Off time
 	}
-
 	return 0;
 }
 
-int readCSVData(void) {
-	FILE *CSVfile;
-	char buffer[16];
-	CSVfile = fopen("/home/pi/Documents/Botny Project/CSV test/CSVtestfile.csv", "r");
-	fgets(buffer, 16, CSVfile);
+void readCSVdata(void) {
+	printf("Reading CSV file:\n");
+	FILE *CSVfile = fopen("/home/pi/Documents/Botny Project/CSV test/CSVtestfile.csv", "r");
+	char buffer[255];
+	fgets(buffer, sizeof(buffer), CSVfile);
+	fclose(CSVfile);
 
-	char *fieldData;
-	fieldData = strtok(buffer, ",");
-	printf("Field data: %s\n", fieldData);
-	strcpy(CSVdata.field, fieldData);
-
-	char *valueData;
-	valueData = strtok(NULL, ",");
-	printf("Value Data: %s\n", valueData);
-
-	CSVdata.value = atoi(valueData);
-
-	return 0;
+	char *delimiter = ",";
+	char *token = strtok(buffer, delimiter);
+	checkToken(token, delimiter);
+	while(token != NULL) {
+		token = strtok(NULL, delimiter);
+		checkToken(token, delimiter);
+	}
+	printf(" Done.\n");
 }
 
+int checkToken(char *token, char *delimiter) {
+	if(token == NULL) {
+		return 0;
+	}
+	else if(strcmp(token, "blinkDelay") == 0) {
+		strcpy(CSVdata.field, token);
+		printf(" Field: %s\n", CSVdata.field);
+
+		token = strtok(NULL, delimiter);
+		CSVdata.value = atoi(token);
+		printf(" Value: %i\n", CSVdata.value);
+	}
+	else {
+		printf(" Error: field unspecified: %s\n", token);
+	}
+	return 0;
+}
